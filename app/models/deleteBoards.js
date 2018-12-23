fa03e485e8deac59fa1f3e9b71bd1d9f9c62f056
aereeeee <dofl5576@gmail.com>
2018-12-23 16:23:53 +0900

    Fix boards models

--- app/models/deleteBoards.js
+++ app/models/deleteBoards.js
@@ -1,26 +1,25 @@
 module.exports = (Sequelize, DataTypes) => {
-    const deleteBoards = Sequelize.define(
-      'deleteBoards',
-      {
-        id: {
-            type: DataTypes.INTEGER, 
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: DataTypes.STRING,
-        createdAt: DataTypes.DATE,
-        updatedAt: DataTypes.DATE,
-        date: DataTypes.DATE,
-        content: DataTypes.STRING,
-        userId: DataTypes.INTEGER,
-        location: DataTypes.STRING,
-        lon: DataTypes.FLOAT,
-        lat: DataTypes.FLOAT,
-        share: DataTypes.STRING,
-        imgUrl: DataTypes.STRING,
+  const deleteBoards = Sequelize.define(
+    'deleteBoards',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
       },
-      {},
-    );
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+      date: DataTypes.DATE,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      location: DataTypes.STRING,
+      lon: DataTypes.FLOAT,
+      lat: DataTypes.FLOAT,
+      share: DataTypes.STRING,
+      imgUrl: DataTypes.STRING,
+    },
+    {},
+  );
 
-    return deleteBoards;
-  };
+  return deleteBoards;
+};
