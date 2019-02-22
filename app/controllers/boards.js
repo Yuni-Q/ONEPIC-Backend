63cee67a5e3b51b5a0a39592a78f80b1b9240a93
Yuni-Q <carnotcycle2@naver.com>
2019-02-23 03:46:37 +0900

    fix

--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -130,7 +130,6 @@ router.get('/users/likes', isLoggedIn, async (req, res) => {
   const result = await db.sequelize.query(query, {
     type: sequelize.QueryTypes.SELECT,
   });
-  console.log(111, result);
   res.json(resultFormat(true, null, result));
 });
 
@@ -232,6 +231,7 @@ router.get('/', isLoggedIn, async (req, res) => {
     totalCount,
     boards,
   };
+  console.log(111, result);
   res.json(resultFormat(true, null, result));
 });
 
