f3e181ec25b19a33303248403893a26acab06bc5
Yuni-Q <carnotcycle2@naver.com>
2019-02-23 12:58:46 +0900

    fix

--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -238,8 +238,10 @@ router.get('/', isLoggedIn, async (req, res) => {
       where boards.createdAt <= '${req.query.date}'
   `;
   console.log('others', others, 'year', year, 'location', location);
-  const query1 = await others === 1 ? `${query} and userId != ${req.user.id}` : `${query} and userId = ${req.user.id}`;
+  console.log(others === 1);
+  const query1 = await others == 1 ? `${query} and userId != ${req.user.id}` : `${query} and userId = ${req.user.id}`;
   console.log(222, others === 1);
+  console.log(others === 1);
   const query2 = await year ? `${query1} and year(Date) = ${year} and month(Date) = ${month}` : query1;
   const query3 = await location ? `${query2} and location = '${location}';` : query2;
   console.log('query1', query1, 'query2', query2, 'query3', query3);
