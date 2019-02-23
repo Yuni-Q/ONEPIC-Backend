bc220535012b3c5cf038ed219dd70ab4ea8605d0
Yuni-Q <carnotcycle2@naver.com>
2019-02-23 13:04:56 +0900

    fix

--- app/controllers/boards.js
+++ app/controllers/boards.js
@@ -239,11 +239,11 @@ router.get('/', isLoggedIn, async (req, res) => {
   `;
   console.log('others', others, 'year', year, 'location', location);
   console.log(others === 1);
-  const query1 = await others == 1 ? `${query} and userId != ${req.user.id}` : `${query} and userId = ${req.user.id}`;
+  const query1 = parseInt(others, 10) ? `${query} and userId != ${req.user.id}` : `${query} and userId = ${req.user.id}`;
   console.log(222, others === 1);
   console.log(others === 1);
-  const query2 = await year ? `${query1} and year(Date) = ${year} and month(Date) = ${month}` : query1;
-  const query3 = await location ? `${query2} and location = '${location}';` : query2;
+  const query2 = parseInt(year, 10) ? `${query1} and year(Date) = ${year} and month(Date) = ${month}` : query1;
+  const query3 = parseInt(location, 10) ? `${query2} and location = '${location}';` : query2;
   console.log('query1', query1, 'query2', query2, 'query3', query3);
   const totalBoards = await db.sequelize.query(query3, {
     type: sequelize.QueryTypes.SELECT,
